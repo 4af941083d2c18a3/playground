a537707627261a7363765c858fbe376cb4e2cebb
RamenSSS <tlsrndEJrtkd@gmail.com>
2024-03-08 13:33:16 +0900

    sch2json

---
+++ schedule2json.js
@@ -0,0 +1,19 @@
+function sch2json() {
+    var title = input('#sch-title')
+    var detail = input('#sch-detail')
+    var time = input('#sch-time')
+    var member = input('#sch-member')
+
+    var text = 
+`            {
+                "title" : ${title},
+                "detail" : ${detail},
+                "time" : ${time},
+                "member" : ${member}
+            }`
+    // var blank = '            '
+
+    navigator.clipboard.writeText(text)
+    $('#that').empty()
+    $('#that').append(`<span>Text is Copied.</span>`)
+}
