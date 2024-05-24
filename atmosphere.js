ad04db61c9817385675d2c993a030e506593ec67
Jinhwang SHIN <161291633+4af941083d2c18a3@users.noreply.github.com>
2024-05-24 21:36:00 +0900

    a

--- atmosphere.js
+++ atmosphere.js
@@ -19,3 +19,36 @@ function feed() {
     }
   })
 }
+
+function searchByDate() {
+  var atLeastOne = false
+  var yymmdd = input('yymmdd')
+  var yy = yymmdd.slice(0,2)
+  var mm = yymmdd.slice(2,4)
+  var dd = yymmdd.slice(4)
+  $('#that').empty()
+
+  fetch(`https://api.cosmo.fans/news/v1/feed?artist=tripleS&limit=32767`,{
+    headers: {'Accept-Language':'ko'}
+  }).then(async (res) => {
+    const data = await res.json()
+    const array = data.results
+
+    array.forEach(item=>{
+      var d = new Date(item.createdAt)
+      if (d.getFullYear() == '20'+yy && d.getMonth()+1 == mm && d.getDate() == dd) {
+        atLeastOne = true
+        var feed_text = item.body
+        var image_list = item.imageUrls
+        $('#that').append(`<p onclick="navigator.clipboard.writeText(feed_text)">${feed_text}</p>`)
+        for (i=0;i<image_list.length;i++) {
+          $('#that').append(`<img src="${image_list[i]}" style="width:100%">`)
+        }
+      }
+    })
+    
+    if (!atLeastOne) {
+      $('#that').append(`<p>해당 날짜의 사진이 없습니다.</p>`)
+    }
+  })
+}
